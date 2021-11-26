cd9bc48b56fbed3f31cead248ec2f8b0afe9a3e5
Felix Rodriguez <= rodriguezfelix17@gmail.com>
2021-11-26 16:00:44 -0400

    add folder in request

--- app/Http/Requests/V1/Admin/Product/CreateProductRequest.php
+++ app/Http/Requests/V1/Admin/Product/CreateProductRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests\V1\Client;
+namespace App\Http\Requests\V1\Admin\Product;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CreateUserRequest extends FormRequest
+class CreateProductRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
--- app/Http/Requests/V1/Admin/User/CreateUserRequest.php
+++ app/Http/Requests/V1/Admin/User/CreateUserRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\V1\Admin;
+namespace App\Http\Requests\V1\Admin\User;
 
 use Illuminate\Foundation\Http\FormRequest;
 

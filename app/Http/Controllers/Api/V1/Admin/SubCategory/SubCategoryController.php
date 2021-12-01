b27413367593fe3bfd249c68a47240b5535285d0
Felix Rodriguez <= rodriguezfelix17@gmail.com>
2021-12-01 11:54:11 -0400

    changes

---
+++ app/Http/Controllers/Api/V1/Admin/SubCategory/SubCategoryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Admin\SubCategory;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SubCategoryController extends Controller
+{
+    //
+}

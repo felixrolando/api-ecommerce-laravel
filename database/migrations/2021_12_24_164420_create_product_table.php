a0f0e15d62cec448a3733a28379acbeb0a51fc28
Felix Rodriguez <rodriguezfelix17@gmail.com>
2021-12-24 12:51:13 -0400

    add table products

--- database/migrations/2021_12_24_164420_create_product_table.php
+++ database/migrations/2021_12_24_164420_create_product_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSubCategoriesTable extends Migration
+class CreateProductTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,15 @@ class CreateSubCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('sub_categories', function (Blueprint $table) {
+        Schema::create('product', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('reference');
+            $table->decimal('price', 8, 2);
             $table->integer('category_id')->unsigned(); 
+            $table->integer('sub_category_id')->unsigned(); 
             $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('sub_category_id')->references('id')->on('sub_categories');
             $table->timestamps();
         });
     }
@@ -29,6 +33,6 @@ class CreateSubCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('sub_categories');
+        Schema::dropIfExists('product');
     }
 }
